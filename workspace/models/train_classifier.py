2abbbbe1ea8d08a2d65a5f78cac07090e0f37e0e
Yifeng <coin123fs@gmail.com>
2020-02-07 19:20:17 -0500

    CPU takes too long to process

--- workspace/models/train_classifier.py
+++ workspace/models/train_classifier.py
@@ -1,24 +1,150 @@
+# import libraries
 import sys
+import nltk
+nltk.download(['punkt', 'wordnet', 'averaged_perceptron_tagger'])
+import re
+import sqlite3
+import numpy as np
+import pandas as pd
+import pickle
+from sqlalchemy import create_engine
+from nltk.tokenize import word_tokenize
+from nltk.stem import WordNetLemmatizer
+from sklearn.metrics import f1_score, precision_score, recall_score
+from sklearn.model_selection import GridSearchCV
+from sklearn.multioutput import MultiOutputClassifier
+from sklearn.ensemble import AdaBoostClassifier
+from sklearn.model_selection import train_test_split
+from sklearn.pipeline import Pipeline
+from sklearn.feature_extraction.text import CountVectorizer, TfidfTransformer
 
+nltk.download(['punkt', 'wordnet', 'averaged_perceptron_tagger'])
 
 def load_data(database_filepath):
-    pass
+    '''
+    Load_data
+    Takes the database filepath to load the data, Runs a query to extract the values for 
+    the X , Y & category names and return them
+
+    @params:
+        database_filepath: the database filepath
+    @return:
+        X: the training data for the project
+        Y: the labels for the project
+        category_names: the name of the categories
+    '''
+
+    # load data from the database
+    engine = create_engine('sqlite:///{}'.format(database_filepath))
+
+    # connect to the databse
+    conn = sqlite3.connect(database_filepath)
+
+    # run a query
+    df = pd.read_sql('SELECT * FROM df', conn)
+
+    # extract values from X and y
+    X =  df['messages']
+    Y = df.iloc[:, 4:]
+
+    category_names = list(Y)
+
+    return X, Y, category_names
 
 
 def tokenize(text):
-    pass
+    '''
+    Tokenize:
+        tokenize the text, stemmatize and lemmatize the text to output a cleaned text
+    @params:
+        text: origin input of the text
+    @return:
 
+    '''
+    # normalize the text
+    text = re.sub(r'[^a-zA-Z0-9]',' ', text.lower())
+
+    words = word_tokenize(text)
+
+    tokens = [w for w in words if w not in stopwords.words("english")]
+
+    # stemming and lemming
+    lemmatizer = WordNetLemmatizer()
+    clean_tokens = []
+
+    for t in tokens:
+        clean_t = lemmatizer.lemmatize(t).strip()
+        clean_tokens.append(clean_t)
+
+    return clean_tokens
 
 def build_model():
-    pass
+    '''
+    Build_model: build a pipeline that can process the text and then performs muilti-output classification
+    on the 36 categories in the database. GridSearchCV is used to find the best parameters for the model
+    
+    @params:
+        None
+    @returns:
+        cv: pipeline used the GridSearchCV method 
+    '''
+    # build the pipeline
+    pipeline = Pipeline([
+        ('text_pipeline', Pipeline([
+            ('vect', CountVectorizer(tokenizer=tokenize)),
+            ('tfidf', TfidfTransformer())
+        ])),
+        ('clf', MultiOutputClassifier(AdaBoostClassifier()))
+    ])
+    # use grid search to find better parameters
+    parameters = {
+        'text_pipeline__tfidf__use_idf': (True, False),
+        'clf__estimator__n_estimators': [10, 50, 100],
+        'clf__estimator__learning_rate': [0.1, 1, 5],
+    }
 
+    cv = GridSearchCV(pipeline, param_grid=parameters)
+
+    return cv
 
 def evaluate_model(model, X_test, Y_test, category_names):
-    pass
+    '''
+    Uses the model to predict and evaluate the results on the 36 categories
+    reports the F1 scores, and recall for each output category of the dataset
+    
+    @params:
+        model: the pipeline we build in the previous function
+        X_test: the testing X inputs
+        Y_test: the ground true labels
+        category_names: the names of the categories
+    
+    @return:
+        return the score
+    '''
+
+    # predict on test data
+    y_pred = model.predict(X_test)
+
+    # report the f1 score, and recall for every category
+    for i in range(36):
+        category = category_names[i]
+        f1 = f1_score(Y_test.iloc[:, i], y_pred.iloc[:, i])
+        precision = precision_score(Y_test.iloc[:, i], y_pred[:, i])
+        recall = recall_score(Y_test.iloc[:, i], y_pred[:, i])
+        print(category)
+        print('\tF1_score: %.4f\tPrecision: %.4f\t Recall: %.4f\n' % (f1, precision, recall))
+
+
 
 
 def save_model(model, model_filepath):
-    pass
+    '''
+    Save_model
+        Saves the model to the model_filepath 
+    '''
+
+    #export the model using pickel
+    pickle.dump(mdoel, open(model_filepath, 'wb'))
 
 
 def main():
